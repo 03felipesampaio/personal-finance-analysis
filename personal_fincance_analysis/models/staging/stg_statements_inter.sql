755609859b9233b94cacba29e829f97456ed8716
Felipe Amorim <03.felipesampaio@gmail.com>
2024-12-18 10:04:31 -0300

    Add stg_statements_inter

---
+++ personal_fincance_analysis/models/staging/stg_statements_inter.sql
@@ -0,0 +1,34 @@
+with source as (
+    select 
+        bank_name,
+        'Bank Statement' as source_type,
+        statement_account_id,
+        statement_start_date,
+        statement_end_date,
+        transaction_date,
+        case
+            when transaction_description like 'Aplicação %' then 'Investment'
+            when transaction_description like 'CDB %' then 'Investment'
+            when transaction_description like 'Inter Conservador Plus FIRF LP' then 'Investment'
+            when transaction_description like 'LCI LIQ 90 DIAS BANCO INTER SA' then 'Investment'
+            when transaction_description like 'Cashback %' then 'Cashback'
+            -- Credito debitado era um serviço do Inter, em que uma compra era feita no crédito, mas o valor era debitado na hora
+            when transaction_description like '% crédito debitado %' then 'Debit card'
+            when transaction_description like 'Débito Automático Fatura Cartão Inter' then 'Bill payment'
+            when transaction_description like 'Pagamento' then 'Bill payment'
+            when transaction_description like 'Pagamento Fatura Cartão Inter' then 'Bill payment'
+            when transaction_description like 'Pagamento efetuado' then 'Bill payment'
+            when transaction_description like 'Pagamento efetuado fatura cartão Inter' then 'Bill payment'
+            when transaction_description like 'Pagamento fatura cartão Inter' then 'Bill payment'
+            when transaction_description like 'Débito automático -%' then 'Automatic debit'
+            when transaction_description like 'Estorno %' then 'Refund'
+            when transaction_description like 'Pix enviado: %' then 'Pix sent'
+            when transaction_description like 'Pix recebido: %' then 'Pix received'
+            else 'Debit card' end
+        as transaction_type,
+        transaction_description,
+        transaction_value,
+        transaction_category 
+    from {{ source('bronze', 'statements__inter') }}
+)
+select * from source
