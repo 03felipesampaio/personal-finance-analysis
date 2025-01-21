621fbba70c71976ee0ef7f98a2118dceb806b351
Felipe Amorim <03.felipesampaio@gmail.com>
2025-01-21 08:18:46 -0300

    Add description from nubank and inter from statements

--- personal_fincance_analysis/models/silver/description_dimension.sql
+++ personal_fincance_analysis/models/silver/description_dimension.sql
@@ -4,17 +4,33 @@ with bills_nubank as (
     select distinct
         ABS(FARM_FINGERPRINT(transaction_description)) as description_id,
         transaction_description as description
-    from {{ ref("stg_bills__nubank") }}
+    from {{ ref("slv_bills_nubank") }}
 ),
 bills_inter as (
     select distinct
         ABS(FARM_FINGERPRINT(transaction_description)) as description_id,
         transaction_description as description
-    from {{ ref("stg_bills_inter") }}
+    from {{ ref("slv_bills_inter") }}
+),
+statements_nubank as (
+    select distinct
+        ABS(FARM_FINGERPRINT(transaction_description)) as description_id,
+        transaction_description as description
+    from {{ ref("slv_statements_nubank") }}
+),
+statements_inter as (
+    select distinct
+        ABS(FARM_FINGERPRINT(transaction_description)) as description_id,
+        transaction_description as description
+    from {{ ref("slv_statements_inter") }}
 ),
 all_sources as (
     select * from bills_nubank
     union all
     select * from bills_inter
+    union all
+    select * from statements_nubank
+    union all
+    select * from statements_inter
 )
 select distinct * from all_sources
