b510d2475dcf4a5295cc8a11bab8be2e6a966c8a
Felipe Amorim <03.felipesampaio@gmail.com>
2025-01-27 11:18:46 -0300

    Add precalculated fields

--- personal_fincance_analysis/models/silver/transactions.sql
+++ personal_fincance_analysis/models/silver/transactions.sql
@@ -59,5 +59,13 @@ all_sources as (
 select 
     -- Essa chave aqui ta errada, exitem compras feitas no mesmo dia, no mesmo lugar e no mesmo valor
     ABS(FARM_FINGERPRINT(CONCAT(transaction_date, source_id, description_id, amount))) as transaction_id,
-    * 
+    *,
+
+    CASE 
+        WHEN amount < 0 THEN 'Expense'
+        ELSE 'Income'
+    END as income_or_expense,
+    ABS(amount) as amount_abs,
+    SUM(amount) OVER (PARTITION BY transaction_date) as daily_balance,
+    SUM(amount) OVER (ORDER BY transaction_date RANGE BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) as daily_cumulative_balance
 from all_sources
