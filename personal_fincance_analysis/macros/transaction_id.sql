eb4e753fe5fdf53a29cf9ef33abfd82c9dad04ba
Felipe Amorim <03.felipesampaio@gmail.com>
2025-01-29 10:51:30 -0300

    Add macro to generate unique transaction IDs

---
+++ personal_fincance_analysis/macros/transaction_id.sql
@@ -0,0 +1,14 @@
+{% macro generate_transaction_id(transaction_date, source_id, description_id, amount) %}
+    abs(
+        farm_fingerprint(
+            concat(
+                {{ transaction_date }}, 
+                {{ source_id }},
+                {{ description_id }},
+                {{ amount }},
+
+                row_number() over (partition by transaction_date, source_id, description_id, amount)
+            )
+        )
+    )
+{% endmacro %}
