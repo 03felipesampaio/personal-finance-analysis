af9dca7ea838e2e3973a5bc96673e06310510330
Felipe Amorim <03.felipesampaio@gmail.com>
2025-01-29 10:52:07 -0300

    Add SQL models for regex mapping and update Nubank bills processing logic

---
+++ personal_fincance_analysis/models/silver/slv_statements_inter_regex_mapping.sql
@@ -0,0 +1,13 @@
+with source as (
+    select 
+        regex_id,
+        regex_pattern,
+        case when place_id is null then -1 else place_id end as place_id,
+        transaction_type,
+        description,
+        active,
+        priority
+    from {{ ref('stg_statements_inter_regex_mapping') }} as mapping
+    left join {{ ref('stg_places') }} as places on mapping.place = places.place
+)
+select * from source
--- personal_fincance_analysis/models/staging/stg_bills__nubank.sql
+++ personal_fincance_analysis/models/staging/stg_bills__nubank.sql
@@ -1,34 +1,45 @@
 with source as (
-    select 
+    select
         bank_name,
-        'Credit Card Bill' as source_type,
         bill_date,
         bill_reference_month,
         bill_value,
         bill_start_date,
         bill_end_date,
         transaction_date,
-        case
-            when transaction_description like 'Antecipada %' then 'Antecipated installment'
-            when transaction_description like '"Credito de' then 'Refund'
-            when transaction_description = 'Crédito de atraso' then 'Late credit'
-            when transaction_description = 'Crédito de parcelamento de compra' then 'Installment credit'
-            when transaction_description like 'Desconto Antecipação %' then 'Antecipated discount'
-            when transaction_description like '"Estorno de %' then 'Refund'
-            when transaction_description like '"IOF de %' then 'Fee'
-            when transaction_description = 'IOF de atraso' then 'Late payment fee'
-            when transaction_description = 'Juros de atraso' then 'Late interest'
-            when transaction_description = 'Multa de atraso' then 'Late fee'
-            when transaction_description like 'Pagamento em %' then 'Bill payment'
-            when transaction_description like '"Parcelamento de compra %' then 'Credit Card'
-            when transaction_description = 'Saldo em atraso' then 'Late balance'
-            -- Tem muita informação no Ifood que dá pra extrair
-            -- when regexp_contains(transaction_description, r' - \d+/\d+') then 'Credit card parcell'
-            else 'Credit card' end
-        as transaction_type,
-        transaction_description,
+        trim(transaction_description) as transaction_description,
         transaction_value,
         transaction_category
     from {{ source('bronze', 'bills__nubank') }}
 )
-select * from source
+select
+    bank_name,
+    'Credit Card Bill' as source_type,
+    bill_date,
+    bill_reference_month,
+    bill_value,
+    bill_start_date,
+    bill_end_date,
+    transaction_date,
+    case
+        when transaction_description like 'Antecipada %' then 'Antecipation installment'
+        when transaction_description = 'Crédito de atraso' then 'Late credit'
+        when transaction_description = 'Crédito de parcelamento de compra' then 'Installment credit'
+        when transaction_description like 'Crédito de "%"' then 'Refund'
+        when transaction_description like 'Desconto Antecipação %' then 'Antecipation discount'
+        when transaction_description like 'Estorno de %' then 'Refund'
+        when transaction_description like 'IOF de %' then 'Fee'
+        when transaction_description = 'IOF de atraso' then 'Late payment fee'
+        when transaction_description = 'Juros de atraso' then 'Late interest'
+        when transaction_description = 'Multa de atraso' then 'Late fee'
+        when transaction_description like 'Pagamento em %' then 'Bill payment'
+        when transaction_description like 'Parcelamento de compra %' then 'Credit card'
+        when transaction_description = 'Saldo em atraso' then 'Late balance'
+        -- Tem muita informação no Ifood que dá pra extrair
+        -- when regexp_contains(transaction_description, r' - \d+/\d+') then 'Credit card parcell'
+        else 'Credit card' end
+    as transaction_type,
+    transaction_description,
+    transaction_value,
+    transaction_category
+from source
