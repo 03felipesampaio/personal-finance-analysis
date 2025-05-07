156d5b911888b62bd98f913c082c8560329db32e
Felipe Sampaio <03.felipesampaio@gmail.com>
2025-05-06 22:17:28 -0300

    add quality metrics

---
+++ personal_finance_analysis/analyses/transactions_silver_quality.sql
@@ -0,0 +1,39 @@
+with source as (
+    select
+        tr.source_id,
+        sr.bank_name,
+        sr.source_type,
+        tr.place_id,
+        tr.category_id,
+        tr.type_id
+    from {{ ref('transactions') }} as tr
+    inner join {{ ref('source_dimension') }} as sr
+        on tr.source_id = sr.source_id
+    {# join {{ ref('source_dimension') }} as sr on tr.source_id = sr.source_id
+    join {{ ref('place_dimension') }} as pl on tr.place_id = pl.place_id
+    join {{ ref('category_dimension') }} as cat on tr.category_id = cat.category_id #}
+)
+
+select
+    bank_name,
+    source_type,
+
+    count(bank_name) as number_of_transactions,
+    count(distinct source_id) as number_of_sources,
+
+    -- places
+    sum(if(place_id != -1, 0, 1)) as missing_places,
+    round(sum(if(place_id != -1, 0, 1)) / count(place_id), 2)
+        as percentage_missing_places,
+
+    -- categories
+    sum(if(category_id != -1, 0, 1)) as missing_categories,
+    round(sum(if(category_id != -1, 0, 1)) / count(category_id), 2)
+        as percentage_missing_categories,
+
+    -- types
+    sum(if(type_id != -1, 0, 1)) as missing_types,
+    round(sum(if(type_id != -1, 0, 1)) / count(type_id), 2)
+        as percentage_missing_types
+from source
+group by bank_name, source_type
